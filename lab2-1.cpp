2ed092d0b6af5fbfa1cf743729fc56f637848ede
sunju0123 <sunjuhwang01@gmail.com>
2016-03-27 18:28:05 +0900

    create repository

---
+++ lab2-1.cpp
@@ -0,0 +1,34 @@
+// Do not remove the include below
+#include "lab2.h"
+
+#define FRONT_LED_PIN 10
+#define REAR_LED_PIN 9
+int ledbright=100;
+//The setup function is called once at startup of the sketch
+void myAnalogWrite(int pin,	int percent,	int time)	{
+//int pin:	pin	number
+//int percent:	brightness	 0%	~	100%
+//int time:	brightness	 remains	the	same	during	 this	time	in	ms
+	for(int i=0;i<time/20;i++)
+	  {
+	  digitalWrite(pin,HIGH);
+	  delay(percent/5);
+	  digitalWrite(pin,LOW);
+	  delay(20-percent/5);
+	  }
+}
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(FRONT_LED_PIN,OUTPUT);
+  pinMode(REAR_LED_PIN,OUTPUT);
+
+}
+
+void loop() {
+	myAnalogWrite(REAR_LED_PIN,ledbright,1000);
+	if(ledbright!=0) ledbright-=25;
+
+//	myAnalogWrite(REAR_LED_PIN,ledbright,100);
+//	if(ledbright!=0) ledbright-=1;
+}
